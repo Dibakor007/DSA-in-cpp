eb2e658dc3db34a6a2cb9d35ff3ce3065af5312f
Dibakor007 <dibakorpramanik007@gmail.com>
2026-01-17 20:19:19 +0600

    Implemented arithmetic operation

---
+++ 3.Arithmetic_operation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 5;
+
+    int c = a + b;
+    int d = a - b;
+    int e = a * b;
+    int f = a / b;
+    int g = a % b;
+    
+    cout << "Addition: " << c << endl;
+    cout << "Subtraction: " << d << endl;
+    cout << "Multiplication: " << e << endl;
+    cout << "Division: " << f << endl;
+    cout << "Modulus: " << g << endl;
+
+    int x = 7;
+    double y = 2;
+    cout << "Result: " << x / y << endl;
+
+    return 0;
+    
+}

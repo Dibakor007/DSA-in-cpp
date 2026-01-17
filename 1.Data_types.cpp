06874f3d0025adade05b9632e6dc61bd6ec60516
Dibakor007 <dibakorpramanik007@gmail.com>
2026-01-17 19:09:09 +0600

    Common primitive data types

---
+++ 1.Data_types.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    char b = 'A';
+    float PI = 3.14f;
+    double d = 3.141592653589793;
+    bool isTrue = true;
+
+    cout << "Integer: " << a << endl;
+    cout << "Character: " << b << endl;
+    cout << "Float: " << PI << endl;
+    cout << "Double: " << d << endl;
+    cout << "Boolean: " << isTrue << endl;
+    return 0;
+    
+
+
+}

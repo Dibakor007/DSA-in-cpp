e0bd8337b898f7127c76cc7a61a6d97c39f789ec
Dibakor007 <dibakorpramanik007@gmail.com>
2026-01-19 14:06:37 +0600

    Logical operations implemented

---
+++ src/5.Logical_operation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    cout << ((5 > 4) || (6 < 2)) << endl;
+    cout << ((5 > 4) && (6 < 2)) << endl;
+    cout << !(5 > 4) << endl;
+
+    return 0;
+    
+}

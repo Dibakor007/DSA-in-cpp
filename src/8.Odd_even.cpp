dc55a17ff9da55522b0b08e7251a004484f7c0df
Dibakor007 <dibakorpramanik007@gmail.com>
2026-01-19 15:20:10 +0600

    Odd even checking function implemented

---
+++ src/8.Odd_even.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a positive number: ";
+    cin >> n;
+
+    if (n % 2 == 0) {
+        cout << n << " is even." << endl;
+    } else {
+        cout << n << " is odd." << endl;
+    }
+}

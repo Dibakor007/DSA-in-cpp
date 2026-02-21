3f9f77ca663e3d7c20ae7c34223b05abfa64ce09
Dibakor007 <dibakorpramanik007@gmail.com>
2026-02-21 17:31:40 +0600

    Fixed use of modifiers in a program

--- src/31.Modifiers.cpp
+++ src/31.Modifiers.cpp
@@ -3,16 +3,27 @@ using namespace std;
 
 int main() {
     int n;
+    cout << "Enter the size of array: ";
     cin >> n;
+    
     cout << "Enter " << n << " integers: ";
     int a[n];
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0) {
+            count++;
+        }
+        cout << a[i] << " ";
+    }
+    cout << "\nTotal even numbers: " << count << "\nNumbers are: ";
     for (int i = 0; i < n; i++) {
         if (a[i] % 2 == 0) {
             cout << a[i] << " ";
         }
     }
+    cout << endl;
     return 0;
 }

4cc7d89252967d5e222f4f9a9de9581bf70c1a95
Dibakor007 <dibakorpramanik007@gmail.com>
2026-04-06 00:33:31 +0600

    Implemented array reversing technique with swap function

---
+++ src/37.Reverse_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+void reverseArray(int arr[], int size) {
+    int start = 0, end = size - 1;
+    while(start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+}
+
+int main() {
+    int arr[] = {5,6,7,3,5,2,1,9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, size);
+
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}

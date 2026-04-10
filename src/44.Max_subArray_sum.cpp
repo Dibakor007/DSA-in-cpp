1b380a6a09af23aa4f0632e2c7c4d2396af9c3b8
Dibakor007 <dibakorpramanik007@gmail.com>
2026-04-11 00:08:08 +0600

    Implemented maximum subArray finding by BruteForce method

---
+++ src/44.Max_subArray_sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main () {
+    int n = 7;
+    int arr[7] = {3, -4, 5, 4, -1, 7, -8};
+
+    int maxSum = INT_MIN;
+
+    for (int st=0; st<n; st++){
+        int curSum = 0;
+        for(int end=st; end<n; end++){
+            curSum += arr[end];
+            maxSum = max(curSum, maxSum);
+        }
+    }
+
+    cout << "Max subarray sum: " << maxSum << endl;
+    return 0;
+}
